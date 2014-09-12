44fdb0818bb90e032e2647e7c0ace238a8310e6b
Daniel Brim <brimizer@gmail.com>
2014-09-11 21:28:48 -0700

    Initial Commit

---
+++ Slidden/Slidden.h
@@ -0,0 +1,19 @@
+//
+//  Slidden.h
+//  Slidden
+//
+//  Created by Daniel Brim on 9/11/14.
+//  Copyright (c) 2014 Daniel Brim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Slidden.
+FOUNDATION_EXPORT double SliddenVersionNumber;
+
+//! Project version string for Slidden.
+FOUNDATION_EXPORT const unsigned char SliddenVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Slidden/PublicHeader.h>
+
+
